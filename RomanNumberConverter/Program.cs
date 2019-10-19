1da7c2a246344e729fdad01ce83a01eac5f3f49d
zav2106 <zav_2106@ngs.ru>
2019-10-19 18:30:32 +0700

    Converter

---
+++ RomanNumberConverter/Program.cs
@@ -0,0 +1,21 @@
+﻿using RomanNumberParser.BusinessObject;
+using RomanNumberParser.Services;
+using System;
+
+namespace RomanNumberParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rNuber = new RomanNumber();
+            rNuber.Number = "MMMCDLXXVIII";
+
+            var converter = new ParserService();
+            int solution = converter.Parse(rNuber);
+
+            Console.WriteLine("This converter is working only with correct format of roman numbers. It does not check the correctness of roman number.");
+            Console.Write("Roman Number: "); Console.Write(rNuber.Number); Console.Write(" is "); Console.Write(solution);
+        }
+    }
+}
