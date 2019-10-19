146beb5a41112cbb033443de55923041c0c8314d
zav2106 <zav_2106@ngs.ru>
2019-10-19 18:09:22 +0700

    Converter

---
+++ BusinessObject/RomanNumber.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RomanNumberParser.BusinessObject
+{
+    public class RomanNumber
+    {
+        public string Number { get; set; }
+    }
+}
---
+++ Interfaces/IParser.cs
@@ -0,0 +1,12 @@
+﻿using RomanNumberParser.BusinessObject;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RomanNumberParser.Interfaces
+{
+    public interface IParser
+    {
+        int Parse(RomanNumber romanNumber);
+    }
+}
